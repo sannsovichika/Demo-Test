b5118ae3dcacd0c7c4da4e93ef8fa094ab983f25
sannsovichika <sannsovichika22@gmail.com>
2023-10-18 21:53:35 +0700

    . Update

--- js/app.js
+++
@@ -1,7 +0,0 @@
-function generateRandomNumber() {
-  // Generate a random number between 1 and 100
-  var randomNumber = Math.floor(Math.random() * 100) + 1;
-
-  // Display the random number
-  document.getElementById("randomNumber").innerText = "Random Number: " + randomNumber;
-}
